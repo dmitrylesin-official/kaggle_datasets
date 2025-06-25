8513ae89a18bb9148fe0d9c8548ed82587bc285b
Dmitry Lesin <lesindima.official@gmail.com>
2025-06-25 19:21:56 +0300

    Create model.py

---
+++ Hate Speech Dataset for Social Media/model.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+from catboost import CatBoostClassifier, Pool
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+import logging
+
+logging.basicConfig(
+  level=logging.INFO,
+  filename='log.txt',
+  filemodel='a',
+  format='%(astime)s - %(levelname)s - %(message)s'
+)
+
+logging.info('Dataset loaded successfully.')
+df = pd.read_csv('hate_speech_1829.csv')
+
+df.head()
+df.info()
+df.isnull().sum()
+
+df = df.drop(['post_id', 'user_id', 'language', 'preprocessed_text'], axis=1)
+
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+df['hour'] = df['timestamp'].dt.hour
+df['day_of_week'] = df['timestamp'].dt.dayofweek
+df['is_weekend'] = df['day_of_week'].isin([5, 6]).astype(int)
+df['month'] = df['timestamp'].dt.month
+df['year'] = df['timestamp'].dt.year
+
+df = df.drop(columns=['timestamp'])
+le_label = LabelEncoder()
+y_encoder = le_label.fit_transform(df['label'])
