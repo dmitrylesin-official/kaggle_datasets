6e7acaec2122f6b20bd0f9dd4b1b59245053e5ab
Dmitry Lesin <lesindima.official@gmail.com>
2025-07-01 06:51:16 +0300

    Update model.py

--- Web Page Phishing/model.py
+++ Web Page Phishing/model.py
@@ -52,7 +52,7 @@ y = df['phishing']
 
 # Split data into train and test sets
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=42
+    X, y, test_size=0.3, stratify=y, random_state=42
 )
 
 # Handle class imbalance
