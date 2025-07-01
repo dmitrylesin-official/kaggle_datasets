c29dd702d8763504a7cd173114a16eb0712a0de0
Dmitry Lesin <lesindima.official@gmail.com>
2025-07-01 06:54:22 +0300

    Update model.py

--- Telecom Churn/model.py
+++ Telecom Churn/model.py
@@ -73,7 +73,7 @@ y = df['Churn']
 # X_new = selector.fit_transform(X, y)
 
 logging.info('Split into train and test. Train size: %d, Test size: %d', len(X_train), len(X_test))
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state=42)
 
 logging.info('Training model: RandomForestClassifier(max_depth=15, n_estimators=100)')
 model = RandomForestClassifier(max_depth=15, n_estimators=100, random_state=42)
